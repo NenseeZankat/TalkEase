8266f568f57584d05cfb45ab0ab5fe28a63dbf03
neha089 <nehapatel30325@gmail.com>
2025-03-11 11:29:37 +0800

    update load model

--- server/db.js
+++ server/db.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB Connected");
+    console.log("✅ MongoDB Atlas Connected Successfully!");
   } catch (err) {
-    console.error(err.message);
+    console.error("❌ MongoDB Connection Error: ", err.message);
     process.exit(1);
   }
 };
